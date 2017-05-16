8c6dd3c53518bf5e3027587babfd39e3a5f54204
Vera Zago <veranyc@gmail.com>
2017-05-16 02:23:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  #definition would go here
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #definition goes here
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
